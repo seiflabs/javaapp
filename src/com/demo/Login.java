cf1f61296eee6061df3fa979d0dbf8524fcfbb4a
SEIF <ubuntu@ip-172-31-22-239.eu-west-3.compute.internal>
2023-04-16 15:42:11 +0000

    correction pom.xml et login.java

--- src/com/demo/Login.java
+++
@@ -1,6 +0,0 @@
-package com.demo;
-class Login {
-    public static void main(String [] args) {
-        System.out.println("Welcome to the demo!");
-    }
-}
---
+++ src/main/java/com/demo/Login.java
@@ -0,0 +1,7 @@
+package com.demo;
+class Login {
+    public static void main(String ...args){
+        System.out.println("My app is running ...");
+    }
+}
+
